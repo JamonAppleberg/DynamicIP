c1caf0bc095509a21cf64c0092d1258f7cab9417
Jane <148792696+JamonAppleberg@users.noreply.github.com>
2023-10-27 16:51:31 +0300

    Update checkweb.sh

--- usr/bin/checkweb.sh
+++ usr/bin/checkweb.sh
@@ -5,7 +5,7 @@ while :;do
     result=$(curl --connect-timeout 3 -Is http://192.168.3.36 | head -1)
     pattern="HTTP/1.1 200 OK";
         if [[ $result =~ $pattern ]]; then              
-            echo "$g is up"
+            echo "connection is up"
 	else 
 c2-ec2 AssociateAddress AllocationId eipalloc-D60B804A NetworkInterfaceId eni-2CB5A482
         fi
