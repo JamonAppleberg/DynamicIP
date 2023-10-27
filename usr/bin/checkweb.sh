c0dab3798021557da162c2019b03c79d6df9df02
Jane <148792696+JamonAppleberg@users.noreply.github.com>
2023-10-27 16:34:25 +0300

    Create checkweb.sh

---
+++ usr/bin/checkweb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source /c2.sh
+PATH=/bin:/bin/bash:/usr/bin:/sbin:/usr/sbin:/usr/local/bin
+while :;do
+    result=$(curl --connect-timeout 3 -Is http://192.168.3.36 | head -1)
+    pattern="HTTP/1.1 200 OK";
+        if [[ $result =~ $pattern ]]; then              
+            echo "$g is up"
+	else 
+c2-ec2 AssociateAddress AllocationId eipalloc-D60B804A NetworkInterfaceId eni-2CB5A482
+        fi
+sleep 7
+done
